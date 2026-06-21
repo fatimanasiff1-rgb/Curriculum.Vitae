e411fa3d11d2286c6bbf19a7cb574d22da4cd4c8
Fatima Nasiff <fatimanasiff1@gmail.com>
2026-06-21 20:21:36 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function toggleContacto (idElemento){
+    const info = document.getElementById(idElemento);
+    if (info){
+        info.classList.toggle('oculto');
+    }
+}
+
+function toggleContacto(idElemento){
+    const info=document.getElementById(idElemento);
+    if (info){
+        info.classList.toggle('oculto');
+    }
+}
